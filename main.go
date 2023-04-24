56d05f13f73574b8973b028c47a203cff1509594
Haydn <haydnmey@gmail.com>
2023-04-24 12:45:36 +0200

    created a slice to hold user info when booking

--- main.go
+++ main.go
@@ -6,12 +6,11 @@ func main() {
 	var conferenceName = "Games Fest"
 	const conferenceTickets = 50
 	var remainingTickets = 50
+	var bookings []string
 
 	fmt.Println("Welcome to Conference bookings!")
 	fmt.Printf("In order to attend %v please purchase a ticket.\n", conferenceName)
 	fmt.Printf("We have %v tickets in total and there are %v left.\n", conferenceTickets, remainingTickets)
-
-	var bookings = [50]string{"Haydn", "peter", "nicole"}
 
 	var firstName string
 	var lastName string
@@ -31,7 +30,10 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets -= userTickets
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. Expect your confirmation email to be sent to %v shortly.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings so far: %v", bookings)
 }
